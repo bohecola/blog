1f9432983f312c9c0a9450f6e470b7ec0dae7d89
bohecola <bohecola@outlook.com>
2023-02-05 23:01:06 +0800

    perf: 暗色模式下图片放大蒙层颜色调整

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -28,5 +28,16 @@
 </template>
 
 <script setup lang="ts">
+import mediumZoom from "medium-zoom";
 import { dateFormat } from "@/utils";
+
+const color = useColorMode();
+
+onMounted(() => {
+  const zoom = mediumZoom("[data-zoomable]");
+
+  watch(color, (newColor) => {
+    zoom.update({ background: newColor.value === "dark" ? "#091a28" : "#fff" });
+  });
+});
 </script>
