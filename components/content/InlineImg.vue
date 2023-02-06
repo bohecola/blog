241ab8fc679fdb8c7b0324e7bce75caaa4d79403
bohecola <bohecola@outlook.com>
2023-02-06 21:46:24 +0800

    feat: markdown 行内图片组件

---
+++ components/content/InlineImg.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+defineProps({
+  src: {
+    type: String,
+    default: ""
+  },
+  alt: {
+    type: String,
+    default: ""
+  },
+  width: {
+    type: [String, Number],
+    default: undefined
+  },
+  height: {
+    type: [String, Number],
+    default: undefined
+  }
+});
+</script>
+
+<template>
+  <img class="inline-img" :src="src" :alt="alt">
+</template>
+
+<style lang="scss" scoped>
+  .inline-img {
+    margin-bottom: -2px;
+    width: 20px;
+    height: 20px;
+  }
+</style>
