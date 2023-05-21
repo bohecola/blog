f9e812ff678c4cb0fbd1ff0f2ccd791a5bfe76f3
bohecola <bohecola@outlook.com>
2023-05-21 23:48:22 +0800

    perf: go back component

--- components/go-back.vue
+++ components/go-back.vue
@@ -1,8 +1,35 @@
+<script setup lang="ts">
+// 路由
+const route = useRoute();
+// 路由器
+const router = useRouter();
+// 是否存在父级路径
+const hasParentPath = computed(() => route.path.lastIndexOf("/") > 0);
+
+// 无效路径
+const invalidPath = ["/blog"];
+
+// 返回上一级
+function goBack () {
+  // 当前路径
+  const currentPath = route.path;
+  // 尾路径分隔符索引
+  const endIndex = currentPath.lastIndexOf("/");
+  // 父级路径
+  const parentPath = currentPath.substring(0, endIndex);
+  // 路径
+  const path = invalidPath.includes(parentPath) ? "/" : parentPath;
+
+  // 路由跳转
+  router.push(path);
+}
+</script>
+
 <template>
   <nuxt-link
-    v-if="$route.path.lastIndexOf('/') > 0"
+    v-if="hasParentPath"
     class="font-mono opacity-50 hover:opacity-75 cursor-pointer"
-    @click="$router.push('/')"
+    @click="goBack"
   >
     cd ..
   </nuxt-link>
