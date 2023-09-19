03e6cbd7439d18ad7005cc1b632c399ef59e22fa
bohecola <bohecola@outlook.com>
2023-09-19 15:22:34 +0800

    fix: remove filing information

--- components/layouts/page/Footer.vue
+++ components/layouts/page/Footer.vue
@@ -2,14 +2,7 @@
   <!-- 底部 -->
   <div class="prose m-auto mb-4">
     <p class="text-sm">
-      <span class="opacity-60 mr-1">© 2021-PRESENT COLAX</span>
-      <nuxt-link
-        class="opacity-60 hover:opacity-75"
-        to="https://beian.miit.gov.cn"
-        target="_blank"
-      >
-        陕ICP备2022001579号-2
-      </nuxt-link>
+      <span class="opacity-60 mr-1">© 2020-PRESENT BOHECOLA</span>
     </p>
   </div>
 </template>
