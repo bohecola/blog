a3c6b16525063c1c16cc17b1932b2b92671f0f3b
bohecola <bohecola@outlook.com>
2023-12-01 00:33:06 +0800

    chore: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,13 @@ module.exports = {
 	],
 	rules: {
 		"quote-props": ["error", "as-needed"],
-		"vue/no-multiple-template-root": "off",
 		"object-curly-newline": ["error", {
 			ObjectExpression: { multiline: true, consistent: true },
 			ObjectPattern: { multiline: true },
 			ImportDeclaration: { multiline: true },
 			ExportDeclaration: { multiline: true, minProperties: 3 }
-		}]
+		}],
+		"vue/no-multiple-template-root": "off",
+		"vue/multi-word-component-names": "off"
 	}
 };
