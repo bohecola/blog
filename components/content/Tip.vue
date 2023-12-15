3387427a2099b39c10cd47bb09f53c6c4c6d6b21
bohecola <bohecola@outlook.com>
2023-12-15 23:58:00 +0800

    feat: tip component

---
+++ components/content/Tip.vue
@@ -0,0 +1,12 @@
+<template>
+  <div
+    class="
+      px-4 border rounded-lg text-sm
+      text-gray-400 dark:text-gray-500
+      border-gray-200 bg-gray-200/25
+      dark:border-gray-600 dark:bg-gray-600/25
+    "
+  >
+    <slot />
+  </div>
+</template>
