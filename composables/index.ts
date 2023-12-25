396cb81214ad09d4e510566e8eb10a2f9c0ac803
bohecola <bohecola@outlook.com>
2023-12-26 03:08:26 +0800

    refactor: composables

--- composables/index.ts
+++ composables/index.ts
@@ -1,3 +1,27 @@
+export function useCopy() {
+	// 拷贝状态
+	const copied = ref(false);
+
+	// 拷贝
+	function copy(code: string, cb?: () => void) {
+		// 开始拷贝
+		copied.value = true;
+		navigator.clipboard.writeText(code)
+			.then(() => {
+				// 拷贝很快，延迟一下再重置拷贝状态
+				setTimeout(() => {
+					cb && cb();
+					copied.value = false;
+				}, 900);
+			});
+	}
+
+	return {
+		copied,
+		copy
+	};
+}
+
 export function useParentPath() {
 	// 虚路径
 	const virtualPath = ["/blog"];
